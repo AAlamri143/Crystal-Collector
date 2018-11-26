c0bb048e0ea1b69ddd20dbf29ba81c0a4845111a
Abdullah Alamri <cast@Abdullahs-MacBook.local>
2018-11-26 00:10:13 -0500

    26Nov

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,96 +1,93 @@
-// $( document ).ready(function(){
-//     var Random=Math.floor(Math.random()*101+19)
-//     // Selects a random number to be shown at the start of the game
-//     // Number should be should be between 19 - 120
-//     //
-//     $('#randomNumber').text(Random);
-//     // Appending random number to the randomNumber id in the html doc
-//     //
-//     var num1= Math.floor(Math.random()*11+1)
-//     var num2= Math.floor(Math.random()*11+1)
-//     var num3= Math.floor(Math.random()*11+1)
-//     var num4= Math.floor(Math.random()*11+1)
-//     // Setting up random numbers for each jewel
-//     // Random number has to be between 1 - 12
-//     // 
-//     var userTotal= 0; 
-//     var wins= 0;
-//     var losses = 0;
-//     //  Decaring variables for tallies
-//   $('#numberWins').text(wins);
-//   $('#numberLosses').text(losses);
-//   //resets the game
-//   function reset(){
-//                         m=Math.floor(Math.random()*101+19);
-//         console.log(Random)
-//         $('#randomNumber').text(Random);
-//         num1= Math.floor(Math.random()*11+1);
-//         num2= Math.floor(Math.random()*11+1);
-//         num3= Math.floor(Math.random()*11+1);
-//         num4= Math.floor(Math.random()*11+1);
-//         userTotal= 0;
-//         $('#finalTotal').text(userTotal);
-//         } 
-//   //adds the wins to the userTotal
-//   function yay(){
-//   alert("You won!");
-//     wins++; 
-//     $('#numberWins').text(wins);
-//     reset();
-//   }
-//   //addes the losses to the userTotal
-//   function loser(){
-//   alert ("You lose!");
-//     losses++;
-//     $('#numberLosses').text(losses);
-//     reset()
-//   }
-//   //sets up click for jewels
-//     $('#one').on ('click', function(){
-//       userTotal = userTotal + num1;
-//       console.log("New userTotal= " + userTotal);
-//       $('#finalTotal').text(userTotal); 
-//             //sets win/lose conditions
-//           if (userTotal == Random){
-//             yay();
-//           }
-//           else if ( userTotal > Random){
-//             loser();
-//           }   
-//     })  
-//     $('#two').on ('click', function(){
-//       userTotal = userTotal + num2;
-//       console.log("New userTotal= " + userTotal);
-//       $('#finalTotal').text(userTotal); 
-//           if (userTotal == Random){
-//             yay();
-//           }
-//           else if ( userTotal > Random){
-//             loser();
-//           } 
-//     })  
-//     $('#three').on ('click', function(){
-//       userTotal = userTotal + num3;
-//       console.log("New userTotal= " + userTotal);
-//       $('#finalTotal').text(userTotal);
-//   //sets win/lose conditions
-//             if (userTotal == Random){
-//             yay();
-//           }
-//           else if ( userTotal > Random){
-//             loser();
-//           } 
-//     })  
-//     $('#four').on ('click', function(){
-//       userTotal = userTotal + num4;
-//       console.log("New userTotal= " + userTotal);
-//       $('#finalTotal').text(userTotal); 
-        
-//             if (userTotal == Random){
-//             yay();
-//           }
-//           else if ( userTotal > Random){
-//             loser();
-//           }
-//     });   
-//   }); 
+
+
+$(document).ready(function(){
+
+  var Random=Math.floor(Math.random()*200+60)
+    $("#randomNumber").text(Random);
+    var num1= Math.floor(Math.random()*15+1)
+    var num2= Math.floor(Math.random()*15+1)
+    var num3= Math.floor(Math.random()*15+1)
+    var num4= Math.floor(Math.random()*15+1)
+    
+    var winsNumber = 0;
+    var lossesNumber = 0;
+    var totalScoreNumber = 0;
+
+
+    $("#numberWins").text(winsNumber);
+    $("#numberLosses").text(lossesNumber);
+    function reset (){
+        Random=Math.floor(Math.random()*102+19);
+        console.log(Random)
+        $('#randomNumber').text(Random);
+        num1= Math.floor(Math.random()*12+1);
+        num2= Math.floor(Math.random()*12+1);
+        num3= Math.floor(Math.random()*12+1);
+        num4= Math.floor(Math.random()*12+1);
+        totalScoreNumber= 0;
+        $('#finalScore').text(totalScoreNumber);
+    }
+         
+    function yay(){
+        alert("You Win")
+        winsNumber++; 
+        $('#numberWins').text(winsNumber);
+        reset();
+    }
+
+    function loser(){
+        alert ("You lose!");
+        lossesNumber++;
+        $('#numberLosses').text(lossesNumber);
+        reset()
+    }
+
+    $('#First').on('click', function(){
+        totalScoreNumber = totalScoreNumber + num1;
+        console.log("New totalScoreNumber= " + totalScoreNumber);
+        $('#finalScore').text(totalScoreNumber); 
+
+        if (totalScoreNumber == Random){
+          yay();
+        }
+        else if (totalScoreNumber > Random){
+          loser();
+        }  
+        Sound = new Audio('../images/rupee.mp3'); 
+        Sound.play();
+
+    })
+    $("#Second").on("click", function(){
+        totalScoreNumber = totalScoreNumber + num2;
+        console.log("New totalScoreNumber= " + totalScoreNumber);
+        $('#finalScore').text(totalScoreNumber); 
+        if (totalScoreNumber == Random){
+          yay();
+        }
+        else if (totalScoreNumber > Random){
+          loser();
+        }   
+    })
+    $("#Third").on("click", function(){
+        totalScoreNumber = totalScoreNumber + num3;
+        console.log("New totalScoreNumber= " + totalScoreNumber);
+        $('#finalScore').text(totalScoreNumber); 
+        if (totalScoreNumber == Random){
+            yay();
+        }
+        else if (totalScoreNumber > Random){
+            loser();
+        }   
+    })
+      $("#Fourth").on("click", function(){
+        totalScoreNumber = totalScoreNumber + num4;
+        console.log("New totalScoreNumber= " + totalScoreNumber);
+      $('#finalScore').text(totalScoreNumber); 
+        if (totalScoreNumber == Random){
+            yay();
+        }
+        else if (totalScoreNumber > Random){
+            loser();
+        }   
+      })
+})
