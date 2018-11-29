ab21c28c82ff3d149be261eb07557768b15cdc7d
Abdullah Alamri <cast@Abdullahs-MacBook.local>
2018-11-29 02:55:45 -0500

    Nov29

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,22 @@
-$(document).ready(function(){
 
-    var Random=Math.floor(Math.random()*200+60)
-    $("#randomNumber").text(Random);
-    var num1= Math.floor(Math.random()*15+1)
-    var num2= Math.floor(Math.random()*15+1)
-    var num3= Math.floor(Math.random()*15+1)
-    var num4= Math.floor(Math.random()*15+1)
-    
+$(document).ready(function () {
+
     var winsNumber = 0;
     var lossesNumber = 0;
     var totalScoreNumber = 0;
 
+    var Random=Math.floor(Math.random()*200+60)
+    console.log(Random);
+    var num1= Math.floor(Math.random()*15+1)
+    console.log(num1);
+    var num2= Math.floor(Math.random()*15+1)
+    console.log(num2);
+    var num3= Math.floor(Math.random()*15+1)
+    console.log(num3);
+    var num4= Math.floor(Math.random()*15+1)
+    console.log(num4);
 
-    $("#numberWins").text(winsNumber);
-    $("#numberLosses").text(lossesNumber);
-    function reset (){
+    function reset(){
         Random=Math.floor(Math.random()*102+19);
         console.log(Random)
         $('#randomNumber').text(Random);
@@ -23,79 +25,62 @@ $(document).ready(function(){
         num3= Math.floor(Math.random()*12+1);
         num4= Math.floor(Math.random()*12+1);
         totalScoreNumber= 0;
-        $('#finalScore').text(totalScoreNumber);
-    }
-         
-    function yay(){
-        
-        // alert("You Win")
-        winsNumber++; 
-        $('#numberWins').text(winsNumber);
-        SoundWin = new Audio('assets/images/win1.mp3'); 
-        SoundWin.play();
-        reset();
+        $('#finalScore').empty();
     }
 
-    function loser(){
-        alert ("You lose!");
-        lossesNumber++;
-        $('#numberLosses').text(lossesNumber);
-        SoundLose = new Audio('assets/images/lose1.mp3'); 
-        SoundLose.play();
-        reset()
+    function hitCrystal() {
+        $("#finalScore").text(totalScoreNumber);
+        $("#picture").html('<img src="assets/images/starting.gif" class="mariopic">');
+        var Sound = new Audio('assets/images/rubee1.mp3');
+        Sound.play();
     }
 
-    $('#First').on('click', function(){
-        totalScoreNumber = totalScoreNumber + num1;
-        console.log("New totalScoreNumber= " + totalScoreNumber);
-        $('#finalScore').text(totalScoreNumber); 
+    $('#randomNumber').text(Random);
 
-        if (totalScoreNumber == Random){
-          yay();
-        }
-        else if (totalScoreNumber > Random){
-          loser();
-        }  
-        Sound = new Audio('assets/images/rubee1.mp3'); 
-        Sound.play();
+    $("#First").on("click", function () {
+        totalScoreNumber += num1; // totalScore = totalScore + coinNum1;
+        hitCrystal();
+        decide();
     })
-    $("#Second").on("click", function(){
-        totalScoreNumber = totalScoreNumber + num2;
-        console.log("New totalScoreNumber= " + totalScoreNumber);
-        $('#finalScore').text(totalScoreNumber); 
-        if (totalScoreNumber == Random){
-          yay();
-        }
-        else if (totalScoreNumber > Random){
-          loser();
-        }   
-        Sound = new Audio('assets/images/rubee1.mp3'); 
-        Sound.play();
+
+    $("#Second").on("click", function () {
+        totalScoreNumber += num2;
+        hitCrystal();
+        decide();
     })
-    $("#Third").on("click", function(){
-        totalScoreNumber = totalScoreNumber + num3;
-        console.log("New totalScoreNumber= " + totalScoreNumber);
-        $('#finalScore').text(totalScoreNumber); 
-        if (totalScoreNumber == Random){
-            yay();
-        }
-        else if (totalScoreNumber > Random){
-            loser();
-        }   
-        Sound = new Audio('assets/images/rubee1.mp3'); 
-        Sound.play();
+
+    $("#Third").on("click", function () {
+        totalScoreNumber += num3;
+        hitCrystal();
+        decide();
     })
-      $("#Fourth").on("click", function(){
-        totalScoreNumber = totalScoreNumber + num4;
-        console.log("New totalScoreNumber= " + totalScoreNumber);
-      $('#finalScore').text(totalScoreNumber); 
-        if (totalScoreNumber == Random){
-            yay();
+
+    $("#Fourth").on("click", function () {
+        totalScoreNumber += num4;
+        hitCrystal();
+        decide();
+    })
+
+    function decide() {
+        if (totalScoreNumber === Random) {
+            winsNumber++;
+            $("#numberWins").text(winsNumber);
+            $("#result").text("You Win !");;
+            $("#picture").html('<img src="assets/images/pictureWin.gif" class="mariopic">');
+            var SoundWin = new Audio('assets/images/win1.mp3');
+            SoundWin.play();
+            reset();
+        } else if (totalScoreNumber > Random) {
+            lossesNumber++;
+            $("#numberLosses").text(lossesNumber);
+            $("#result").text("Oh No !");
+            $("#picture").html('<img src="assets/images/pictureLoss.gif" class="mariopic">');
+            var SoundLose = new Audio('assets/images/lose1.mp3');
+            SoundLose.play();
+            reset();
+        } else {
+            $("#result").text("");
         }
-        else if (totalScoreNumber > Random){
-            loser();
-        }   
-        Sound = new Audio('assets/images/rubee1.mp3'); 
-        Sound.play();
-      })
-})
+    }
+
+});
